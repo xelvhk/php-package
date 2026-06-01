7ca374f160cbcf5bcadd46f8d77bc4e5b9536715
Lex <166012830+xelvhk@users.noreply.github.com>
2026-06-01 13:18:54 +0300

    fix(ci): add scalar types for phpstan

--- src/Utils.php
+++ src/Utils.php
@@ -3,7 +3,7 @@
 namespace Hexlet\Phpunit\Utils;
 
 // Эта функция переворачивает переданную строку
-function reverseString($string)
+function reverseString(string $string): string
 {
     return implode(array_reverse(str_split($string)));
 }
